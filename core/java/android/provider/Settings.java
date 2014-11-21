b3f7f610b6efac5ba705d9e8a636193bd1b036fb
Lars Greiss <kufikugel@googlemail.com>
2014-11-21 22:40:46 +0100

    Frameworks: Add reboot menu - prepare for Slims Global menu - misc changes
    Adding advanced reboot menu like it was on kk4.4
    Adding advanced reboot secure option
    Move reboot into safe mode from longpress power to longpress reboot
    Enable for now per default Airplane mode and sound and reboot till we have imported our SlimActions class to make in configurable
    
    misc changes and adaptions to lp
    
    Change-Id: Idd4f56d985ac51ade5f8deb2597d8f1dda22088a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5735,6 +5735,12 @@ public final class Settings {
         public static final String ASSIST_SCREENSHOT_ENABLED = "assist_screenshot_enabled";
 
         /**
+         * Whether to include options in power menu for rebooting into recovery and bootloader
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * Names of the service components that the current user has explicitly allowed to
          * see all of the user's notifications, separated by ':'.
          *
@@ -5937,6 +5943,7 @@ public final class Settings {
             DOUBLE_TAP_TO_WAKE,
             CAMERA_GESTURE_DISABLED,
             PRIVACY_GUARD_DEFAULT,
+            ADVANCED_REBOOT
         };
 
         /**
