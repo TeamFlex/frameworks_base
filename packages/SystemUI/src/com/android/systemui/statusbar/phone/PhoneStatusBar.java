08de2e619385af6cea7f3ae1dbb3c13e766a79f1
Lars Greiss <kufikugel@googlemail.com>
2015-02-27 09:11:44 +0000

    Add user configurable shortcuts [1/2]
    ***THIS IS NOT THE COMPLETE COMMIT !!***
    This was added to complement Fling source, please see this for full commit:
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/e50e8be908a5b7be437c46e2f109f80f197434ea
    
    ** DU Edits **
    - Removed assist shortcut for now
    
    Change-Id: I0ba960853b220243cedfe1e418a280d8222d5d7f
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	core/java/com/android/internal/statusbar/IStatusBar.aidl
    	core/java/com/android/internal/statusbar/IStatusBarService.aidl
    	packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
    	packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
    	services/core/java/com/android/server/statusbar/StatusBarManagerService.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2533,6 +2533,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         checkBarModes();
     }
+	
+	@Override // CommandQueue
+    public void showCustomIntentAfterKeyguard(Intent intent) {
+        startActivityDismissingKeyguard(intent, false, false);
+    }
 
     @Override // CommandQueue
     public void setWindowState(int window, int state) {
