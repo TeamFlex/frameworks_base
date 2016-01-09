a769d57168df926b5a259541c0a5565327c04afe
paulb-nl <paul@xyz.com>
2016-01-09 01:00:16 -0500

    base: Show Optimizing app dialog on first boot

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -6182,7 +6182,6 @@ public class PackageManagerService extends IPackageManager.Stub {
         if (DEBUG_DEXOPT) {
             Log.i(TAG, "Optimizing app " + curr + " of " + total + ": " + pkg.packageName);
         }
-        if (!isFirstBoot()) {
             try {
                 // give the packagename to the PhoneWindowManager
                 ApplicationInfo ai;
@@ -6198,7 +6197,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                                 curr, total), true);
             } catch (RemoteException e) {
             }
-        }
+
         PackageParser.Package p = pkg;
         synchronized (mInstallLock) {
             mPackageDexOptimizer.performDexOpt(p, null /* instruction sets */,
